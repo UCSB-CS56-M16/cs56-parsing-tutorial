82ed89849dc217f445e4d7b2e171c11ec8aa27f2
Kyle Dewey <kyledewey@cs.ucsb.edu>
2016-07-11 16:05:54 -0700

    Added q/quit functionality for step04

--- step04/src/edu/ucsb/cs56/pconrad/parsing/Main.java
+++ step04/src/edu/ucsb/cs56/pconrad/parsing/Main.java
@@ -3,18 +3,33 @@ package edu.ucsb.cs56.pconrad.parsing;
 import java.util.Scanner;
 
 public class Main {
-    public static void handleInput(final String input) {
-	if (new Parser(input).syntacticallyValid()) {
-	    System.out.println("Parses");
-	} else {
-	    System.out.println("Does not parse");
-	}
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean shouldExit(final String input) {
+        final String trimmed = input.trim();
+        return trimmed.equals("q") || trimmed.equals("quit");
+    }
+    
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean handleInput(final String input) {
+        if (shouldExit(input)) {
+            return true;
+        } else {
+            if (new Parser(input).syntacticallyValid()) {
+                System.out.println("Parses");
+            } else {
+                System.out.println("Does not parse");
+            }
+            return false;
+        }
     }
     
     public static void main(String[] args) {
 	final Scanner input = new Scanner(System.in);
-	while (input.hasNextLine()) {
-	    handleInput(input.nextLine());
-	}
+	while (input.hasNextLine() &&
+               !handleInput(input.nextLine())) {}
     }
 }
