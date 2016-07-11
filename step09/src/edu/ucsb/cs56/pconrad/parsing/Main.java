a9aae584e24d0ba0b7253e3f1a36cf79fa21be53
Kyle Dewey <kyledewey@cs.ucsb.edu>
2016-07-11 16:19:03 -0700

    Added q/quit functionality

--- step09/src/edu/ucsb/cs56/pconrad/parsing/Main.java
+++ step09/src/edu/ucsb/cs56/pconrad/parsing/Main.java
@@ -9,23 +9,38 @@ import java.util.Scanner;
 public class Main {
     public static AST parse(final String input)
 	throws TokenizerException, ParserException {
-	return DefaultInterpreterInterface.DEFAULT.tokenizeAndParse(input);
+        return DefaultInterpreterInterface.DEFAULT.tokenizeAndParse(input);
+    }
+
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean shouldExit(final String input) {
+        final String trimmed = input.trim();
+        return trimmed.equals("q") || trimmed.equals("quit");
     }
     
-    public static void handleInput(final String input) {
-	try {
-	    System.out.println(parse(input));
-	} catch (TokenizerException e) {
-	    System.out.println("Failed to tokenize: " + e.getMessage());
-	} catch (ParserException e) {
-	    System.out.println("Failed to parse: " + e.getMessage());
-	}
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean handleInput(final String input) {
+        if (shouldExit(input)) {
+            return true;
+        } else {
+            try {
+                System.out.println(parse(input));
+            } catch (TokenizerException e) {
+                System.out.println("Failed to tokenize: " + e.getMessage());
+            } catch (ParserException e) {
+                System.out.println("Failed to parse: " + e.getMessage());
+            }
+            return false;
+        }
     }
     
     public static void main(String[] args) {
 	final Scanner input = new Scanner(System.in);
-	while (input.hasNextLine()) {
-	    handleInput(input.nextLine());
-	}
+	while (input.hasNextLine() &&
+               !handleInput(input.nextLine())) {}
     }
 }
