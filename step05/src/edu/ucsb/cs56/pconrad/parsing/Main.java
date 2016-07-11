37fff6fb0148cd8cf7e11443d639e063e073b596
Kyle Dewey <kyledewey@cs.ucsb.edu>
2016-07-11 16:15:44 -0700

    Added q/quit capability

--- step05/src/edu/ucsb/cs56/pconrad/parsing/Main.java
+++ step05/src/edu/ucsb/cs56/pconrad/parsing/Main.java
@@ -5,18 +5,33 @@ import edu.ucsb.cs56.pconrad.parsing.parser.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void handleInput(final String input) {
-	try {
-	    System.out.println(new Parser(input).parse());
-	} catch (ParserException e) {
-	    System.out.println("Does not parse");
-	}
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean shouldExit(final String input) {
+        final String trimmed = input.trim();
+        return trimmed.equals("q") || trimmed.equals("quit");
+    }
+    
+    /**
+     * Returns true if it should exit
+     */
+    public static boolean handleInput(final String input) {
+        if (shouldExit(input)) {
+            return true;
+        } else {
+            try {
+                System.out.println(new Parser(input).parse());
+            } catch (ParserException e) {
+                System.out.println("Does not parse");
+            }
+            return false;
+        }
     }
     
     public static void main(String[] args) {
 	final Scanner input = new Scanner(System.in);
-	while (input.hasNextLine()) {
-	    handleInput(input.nextLine());
-	}
+	while (input.hasNextLine() &&
+               !handleInput(input.nextLine())) {}
     }
 }
